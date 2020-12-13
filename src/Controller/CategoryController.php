e943d00253cbbdc0f42c0341c7d7223ba9cd8399
Matthieu <bardetmatthieu@posteo.net>
2020-12-13 12:53:47 +0100

    finished quest 11

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,13 +7,14 @@ use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class CategoryController
  * @package App\Controller
  * @Route ("/categories", name="category_")
  */
-
 class CategoryController extends AbstractController
 {
     /**
@@ -31,12 +32,42 @@ class CategoryController extends AbstractController
     }
 
     /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', ["form" => $form->createView()]);
+    }
+
+    /**
      * @Route("/{categoryName}", name="show")
      * @param string $categoryName
      * @return Response
      */
 
-    public function show(string $categoryName) :Response
+    public function show(string $categoryName): Response
     {
         $category = $this->getDoctrine()
             ->getRepository(Category::class)
